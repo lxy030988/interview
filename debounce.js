2245998828aabe85b18be3cdcb9aacbfb9a769ac
lixiangyu <654762937@qq.com>
2020-11-06 10:01:41 +0800

    防抖 节流

---
+++ debounce.js
@@ -0,0 +1,10 @@
+//防抖
+function debounce(fn, delay = 500) {
+  let timer = null;
+  return function (...args) {
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
---
+++ throttle.js
@@ -0,0 +1,42 @@
+//节流
+function throttle(fn, delay = 500) {
+  let flag = true;
+  return function (...args) {
+    if (!flag) return;
+    flag = false;
+    setTimeout(() => {
+      fn.apply(this, args);
+      flag = true;
+    }, delay);
+  };
+}
+
+function throttle2(fn, delay = 500) {
+  let preTime = Date.now();
+  return function (...args) {
+    const nowTime = Date.now();
+    if (nowTime - preTime >= delay) {
+      fn.apply(this, args);
+      preTime = Date.now();
+    }
+  };
+}
+
+function throttle3(fn, delay = 500) {
+  let preTime = Date.now(),
+    timer = null;
+  return function (...args) {
+    const nowTime = Date.now();
+    if (nowTime - preTime >= delay) {
+      fn.apply(this, args);
+      preTime = Date.now();
+    } else {
+      //时间还没到
+      clearTimeout(timer);
+      setTimeout(() => {
+        preTime = Date.now();
+        fn.apply(this, args);
+      }, delay);
+    }
+  };
+}
