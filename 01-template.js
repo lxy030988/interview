b063e52d544fb1bacdafa165a178965864c342dd
lixiangyu <654762937@qq.com>
2021-08-24 10:50:23 +0800

    /(\w)\1+/

--- 01-template.js
+++ 01-template.js
@@ -18,12 +18,12 @@ module.exports = {
   },
 };
 
-let template = '我是{{name}}，年龄{{age}}，性别{{sex}}';
-let data = {
-  name: '姓名',
-  age: 18
-}
-render(template, data); // 我是姓名，年龄18，性别undefined
+// let template = '我是{{name}}，年龄{{age}}，性别{{sex}}';
+// let data = {
+//   name: '姓名',
+//   age: 18
+// }
+// render(template, data); // 我是姓名，年龄18，性别undefined
 
 // function render(template, data) {
 //   const reg = /\{\{(\w+)\}\}/; // 模板字符串正则
@@ -36,21 +36,27 @@ render(template, data); // 我是姓名，年龄18，性别undefined
 //   return template; // 如果模板没有模板字符串直接返回
 // }
 
-function render(template, data) {
-  //   /\{\{(\w+)\}\}/ === /\{\{(.+?)\}\}/
-  const reg = /\{\{(\w+)\}\}/g
-  template = template.replace(reg, function (x) {
-    console.log(arguments, x)
-    return data[arguments[1]]
-  })
-  console.log(template)
-  // console.log(reg.exec(template))
-}
+// function render(template, data) {
+//   //   /\{\{(\w+)\}\}/ === /\{\{(.+?)\}\}/
+//   const reg = /\{\{(\w+)\}\}/g
+//   template = template.replace(reg, function (x) {
+//     console.log(arguments, x)
+//     return data[arguments[1]]
+//   })
+//   console.log(template)
+//   // console.log(reg.exec(template))
+// }
 
-let s1 = "get-element-by-id"
+// let s1 = "get-element-by-id"
 
-s1 = s1.replace(/-\w/g, function (x) {
-  console.log(x)
-  return x[1].toUpperCase()
+// s1 = s1.replace(/-\w/g, function (x) {
+//   console.log(x)
+//   return x[1].toUpperCase()
+// })
+// console.log(s1)
+
+// 查找字符串中出现最多的字符和个数  先排序再正则匹配
+let s2 = 'aaabbbbbcccccccc'
+s2.replace(/(\w)\1+/g, function () {
+  console.log(arguments)
 })
-console.log(s1)
