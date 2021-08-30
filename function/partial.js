863e671a728296ac3a0b5ed77a2d9fb2fd36d05c
lixiangyu <654762937@qq.com>
2021-08-30 17:10:07 +0800

    partial2

--- function/partial.js
+++ function/partial.js
@@ -12,3 +12,18 @@ const add1 = partial(add, 1)
 const add2 = partial(add, 1, 2)
 console.log(add1(2, 3))
 console.log(add2(3))
+
+
+const partial2 = (fn, ...args) => {
+
+  return (...p) => {
+    args[0] = p[0]
+    p = p.slice(1)
+    return fn(...args, ...p)
+  }
+}
+function clg(a, b, c) {
+  console.log(a, b, c)
+}
+let partialClg = partial2(clg, '_', 2)
+partialClg(1, 3)  // 依次打印：1, 2, 3
